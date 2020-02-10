d6f51fb4f88b28d3d6f735b9e79665a2c0d7d79e
Hemanth <hemanthkotla@icloud.com>
2020-02-09 19:01:16 -0500

    Added Player Movement and Joystick Movement

---
+++ PKH-SpaceJump/PlayerState.swift
@@ -0,0 +1,22 @@
+//
+//  PlayerState.swift
+//  PKH-SpaceJump
+//
+//  Created by Hemanth Kotla on 2020-02-09.
+//  Copyright © 2020 Hemanth Kotla. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+fileprivate let characterAnimation = "sprit Animation"
+
+class PlayerState: GKState {
+    
+    unowned var playerNode : SKNode
+    
+    override init( playerNode : SKNode ) {
+         
+    }
+
+}
